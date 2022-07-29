f60255d9e9168be10f78b0ff3c41b6a4bb7f58ab
Fernando <fernando_souza_vasconcelos@hotmail.com>
2022-07-29 09:26:14 -0300

    Validações iniciais

--- main_api.py
+++ main_api.py
@@ -15,16 +15,31 @@ def home():
 @app.route('/api/task/<int:task_id>', methods = ['GET'])
 def api_return(task_id):
     task = tasks[task_id]
-    entrega_ninja = {
-        "nome": "Entrega Ninja",
-        "valor_frete": (task['peso'] * 0.3) / 10,
-        "prazo_dias": 6
-    }
-    entrega_kabum = {
-        "nome": "Entrega KaBuM",
-        "valor_frete": (task['peso'] * 0.2) / 10,
-        "prazo_dias": 4
-    }
+    print(task)
+    if(10 < task['dimensao']['altura'] < 200) and (6 < task['dimensao']['largura'] < 140):
+        entrega_ninja = {
+            "nome": "Entrega Ninja",
+            "valor_frete": (task['peso'] * 0.3) / 10,
+            "prazo_dias": 6
+        }
+    else:
+        entrega_ninja = {
+            "nome": "Entrega Ninja",
+            "status": "Dimensões insuficientes"
+            }
+
+    if(5 < task['dimensao']['altura'] < 140) and (13 < task['dimensao']['largura'] < 125):
+        entrega_kabum = {
+            "nome": "Entrega KaBuM",
+            "valor_frete": (task['peso'] * 0.2) / 10,
+            "prazo_dias": 4
+        }
+    else:
+        entrega_kabum = {
+            "nome": "Entrega KaBuM",
+            "status": "Dimensões insuficientes"
+            }
+
     return jsonify([entrega_ninja, entrega_kabum])
 
 @app.route('/api/task', methods=['POST'])
