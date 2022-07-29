5ec6df34a0102f38ec0584774e835f09bcf55362
Fernando <fernando_souza_vasconcelos@hotmail.com>
2022-07-29 09:13:08 -0300

    POST Method

--- main_api.py
+++ main_api.py
@@ -8,23 +8,34 @@ tasks = []
 def home():
     return 'The API is running...'
 
-@app.route('/api/task', methods = ['GET'])
-def api_return():
-    return jsonify(tasks)
+# @app.route('/api/task', methods = ['GET'])
+# def api_return():
+#     return jsonify(tasks)
 
 @app.route('/api/task/<int:task_id>', methods = ['GET'])
 def api_return(task_id):
-    return jsonify(tasks[task_id])
+    task = tasks[task_id]
+    entrega_ninja = {
+        "nome": "Entrega Ninja",
+        "valor_frete": (task['peso'] * 0.3) / 10,
+        "prazo_dias": 6
+    }
+    entrega_kabum = {
+        "nome": "Entrega KaBuM",
+        "valor_frete": (task['peso'] * 0.2) / 10,
+        "prazo_dias": 4
+    }
+    return jsonify([entrega_ninja, entrega_kabum])
 
 @app.route('/api/task', methods=['POST'])
 def create_task():
-    if not request.json or not 'title' in request.json:
+    if not request.json or not 'peso' in request.json:
         return(400)
     task = {
         'dimensao': {
-            'altura': request.json['altura'],
-            'largura': request.json['largura']
-            },
+            'altura':request.json['dimensao']['altura'],
+            'largura':request.json['dimensao']['largura']
+        },
         'peso': request.json['peso']
     }
     tasks.append(task)
